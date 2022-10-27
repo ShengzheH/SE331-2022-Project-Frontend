dd384cff4af1c1c3de88a0b4eb5ffa6856d7604d
HaolongHuai <1612907625@qq.com>
2022-10-27 16:44:34 +0800

    update doctor view

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <label v-if="label">{{ label }}</label>
+  <input
+    v-bind="$attrs"
+    :value="modelValue"
+    :placeholder="label"
+    @input="inputHandler($doctor)"
+    class="field"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  },
+  methods: {
+    inputHandler(doctor) {
+      this.$emit('update:modelValue', doctor.target.value)
+    }
+  }
+}
+</script>
---
+++ src/views/DoctorCommentView.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <h1>Please leave your comments!</h1>
+    <form @submit.prevent="saveEvent">
+      <BaseInput
+        v-model="doctor.category"
+        type="text"
+        label="Category"
+        class="field"
+      />
+
+      <h3>Name & describe your event</h3>
+
+      <BaseInput v-model="doctor.comments" type="text" label="Comments" />
+      <button type="submit">Submit</button>
+    </form>
+
+    <pre>{{ event }}</pre>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+import UploadImages from 'vue-upload-drop-images'
+export default {
+  inject: ['GStore'],
+  components: {
+    UploadImages
+  },
+  data() {
+    return {
+      doctor: {
+        category: '',
+        title: '',
+        description: '',
+        location: '',
+        organizer: { id: '', name: '' },
+        imageUrls: []
+      },
+      files: []
+    }
+  },
+  methods: {
+    saveEvent() {
+      Promise.all(
+        this.files.map((file) => {
+          return EventService.updateFile(file)
+        })
+      )
+        .then((response) => {
+          this.event.imageUrls = response.map((r) => r.data)
+          EventService.saveEvent(this.event).then((response) => {
+            console.log(response)
+            this.$router.push({
+              name: 'EventDetails',
+              params: { id: response.data.id }
+            })
+          })
+          this.GStore.flashMessage =
+            'You are succcessfully add a new event for ' + response.data.title
+          setTimeout(() => {
+            this.GStore.flashMessage = ''
+          }, 3000)
+        })
+        .catch(() => {
+          this.$router.push('NetworkError')
+        })
+    }
+  }
+}
+</script>
+<style scoped></style>
