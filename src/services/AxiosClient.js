90f8e4935064778ec3684fa20442272a339a3c16
ShengzheH <shengzhehuang15@gmail.com>
2022-10-31 00:10:38 +0800

    deploy

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_BACKEND_URL,
+  baseURL: 'http://52.91.186.96:8999',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
